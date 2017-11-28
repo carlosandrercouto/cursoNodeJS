ce9f6c2afbdbd2ab2f8a9e9e10d246b431d98832
Carlos André Couto <carlosandrecouto@live.com>
2017-11-27 23:20:26 -0200

    aula 13

---
+++ app.js
@@ -0,0 +1,16 @@
+var express =  require('express');
+var app =  express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res){
+	res.send("<html><body><h1>Portal de Noticias</h1><body></html>");
+});
+
+app.get('/tecnologia', function(req, res){
+	res.render("secao/tecnologia");
+});
+
+app.listen(3000, function(){
+	console.log("Servidor rodando com express");
+});
