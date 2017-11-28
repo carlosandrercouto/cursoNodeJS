6d0f92beda69eb80468782b216a32a8eeae36b05
Carlos André Couto <carlosandrecouto@live.com>
2017-11-28 20:01:09 -0200

    Aula 16

--- app.js
+++ app.js
@@ -4,13 +4,17 @@ var app =  express();
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res){
-	res.send("<html><body><h1>Portal de Noticias</h1><body></html>");
+		res.render("home/index");
 });
 
-app.get('/tecnologia', function(req, res){
-	res.render("secao/tecnologia");
+app.get('/admin', function(req, res){
+	res.render("admin/form_add_noticia");
+});
+
+app.get('/noticia', function(req, res){
+	res.render("noticias/noticias");
 });
 
 app.listen(3000, function(){
 	console.log("Servidor rodando com express");
-}); 
+});
